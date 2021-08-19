0b42ea1f98f102d037acca2d1a295d5c8dc80cd4
anbrne <87954679+anbrne@users.noreply.github.com>
2021-08-19 17:18:15 -0300

    Upload Programa de Notas musicais

---
+++ Enum/Notas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotasMusicais.Enum
+{
+    public enum Notas
+    {
+
+        _do = 0,
+        _re = 1,
+        _mi = 2,
+        _fa = 3,
+        _sol = 4,
+        _la = 5,
+        _si = 6
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace NotasMusicais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroInicial = 0;
+            int numeroFinal = 0;
+
+            Console.WriteLine("************** Notas Musicais ************");
+
+            Console.WriteLine("Digite o número inicial:");
+            numeroInicial = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o número final:");
+            numeroFinal = Convert.ToInt32(Console.ReadLine());
+
+
+            int totalImprimir = numeroFinal - numeroInicial;
+            int contadorNotas = numeroInicial;
+
+            while (contadorNotas < numeroFinal)
+            {
+                int notaAtual = contadorNotas;
+                if (contadorNotas > 6)
+                {
+                    notaAtual = notaAtual - 7;
+                }
+
+                Console.Write(" " + labelNotaMusical(notaAtual) + " ");
+                contadorNotas++;
+            }
+        }
+
+        public static string labelNotaMusical(int notaMusical)
+        {
+            switch (notaMusical)
+            {
+                case 0:
+                    return "Dó";
+                    
+                case 1:
+                    return "Ré";
+                case 2:
+                    return "Mi";
+                case 3:
+                    return "Fá";
+                case 4:
+                    return "Sol";
+                case 5:
+                    return "Lá";
+                case 6:
+                    return "Si";
+                default:
+                    return "Não musícal não encontrada";
+            }
+        }
+    }
+}
